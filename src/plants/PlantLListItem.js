f0f004c3dac2457231e5c39875c9866e98c7bde1
Luke <lukemoore237@gmail.com>
2024-09-09 20:16:46 -0700

    Renamed plant list item file.

--- src/plants/PlantLListItem.js
+++ src/plants/PlantLListItem.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
+import '../styles/PlantListItem.css';
 
-export function Plant( { plant }) {
+export function PlantListItem( { plant }) {
     const photoURL = plant.photos[0].url;
     const replacePhotoSize = "square";
     const newPhotoSize = "medium";
@@ -11,8 +12,11 @@ export function Plant( { plant }) {
     return (
         <li>
             <Link to={`/plant/${plant.taxon.id}`} className="plant-page-link">
-                <h3>{ plant.taxon.name } - <i>{ plant.taxon.preferred_common_name }</i></h3>
-                <img src={newURL} alt={`${plant.taxon.name} in its habitat`} />
+                <img className='plant-img-square' src={newURL} alt={`${plant.taxon.name} in its habitat`} />
+                <div className="plant-names">
+                    <h3>{ plant.taxon.name }</h3>
+                    <h4><i>{ plant.taxon.preferred_common_name }</i></h4>
+                </div>
             </Link>
         </li>
     );
