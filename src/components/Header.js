6cc52a4010497bb47c57f99c82d320c644bc5b35
Luke <lukemoore237@gmail.com>
2024-09-09 20:18:08 -0700

    Moved heading into separate component.

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import '../styles/Header.css'
+
+export function Header() {
+    return (
+        <header>
+            <div role="banner">
+                <h1>Luke's Botany Blog</h1>
+            </div>
+            <p>
+                Exlpore recent plant observations I've posted to the iNaturalist platform. 
+                If you live in California, where I currently am, you might have even seen some of these plants yourself!
+                Learning about native plants enhances your connection to the natural world and makes outdoor experiences more meaningful and engaging.
+            </p>
+            <hr></hr>
+        </header>
+    );
+}
